5f93c81533c86aa8c53c3b797f93eca977c26d4d
Dario Salerno <dsalerno@emtrica.com>
2021-12-20 16:50:49 +0100

    Added dynamic config

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="py-streamkompressor",
+    version="0.0.1",
+    author="Dario Salerno",
+    author_email="info@dariosalerno.com",
+    description="IoT Sample Stream Compressor",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sigsalerno/py-streamkompressor",
+    project_urls={
+        "Bug Tracker": "https://github.com/sigsalerno/py-streamkompressor/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires=[
+        "msgpack"
+    ]
+)
