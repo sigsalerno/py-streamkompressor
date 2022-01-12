5bd5697926f8e386cd69dea58fb8ed9170407a19
Dario Salerno <dsalerno@emtrica.com>
2022-01-12 14:15:52 +0100

    v0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="py-streamkompressor",
-    version="0.0.1",
+    version="0.0.2",
     author="Dario Salerno",
     author_email="info@dariosalerno.com",
     description="IoT Sample Stream Compressor",
